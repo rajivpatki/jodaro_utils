208391abeba3a6eb7b398a8221f5a89b4ff823c9
Rajiv Patki <rajivpatki1@gmail.com>
2023-11-02 13:24:21 +0530

    comments and fixes

--- utils/file_ops.py
+++ utils/file_ops.py
@@ -48,14 +48,21 @@ def list_s3(folder_path):
     else:
         return []
 
-def listdir(folder_path):
-    """Returns a list of full paths to the contents of a specified location, accepts s3
+def listdir(folder_path: str) -> list[str]:
+    """
+    Returns a list of full paths to the contents of a specified location, accepts s3
+
+    Args:
+    - folder_path (str): path to the folder to list contents of
+
+    Returns:
+    - list of str: list of full paths to the contents of the specified location
     """
     if 's3://' in folder_path:
         folder_path = folder_path.replace('\\', '/')
         # create S3 client
+        s3 = boto3.client('s3')
         # specify bucket and prefix (folder path)
-        # bucket_name = 'boulevarddataplatform'
         bucket_name, prefix = _bucket_prefix(folder_path)
         if prefix[-1] != '/':
             prefix += '/'
@@ -79,14 +86,11 @@ def listdir(folder_path):
     else:
         return [os.path.join(folder_path, _) for _ in os.listdir(folder_path)]
 
-def touch_file(file_path):
+def touch_file(file_path: str) -> None:
     """Creates an empty file if it does not exist
     """
     if 's3://' in file_path:
         file_path = file_path.replace('\\', '/')
-        # s3 = boto3.client('s3')
-        # Extract bucket name
-        # bucket_name = re.search(r's3://([^/]+)/', file_path).group(1)
         bucket_name, prefix = _bucket_prefix(file_path)
 
         # Extract key (object key)
@@ -99,10 +103,10 @@ def touch_file(file_path):
         with open(file_path, 'w') as document:
             pass
 
-def touch_folder(folder_path):
+def touch_folder(folder_path:str) -> None:
     """Creates an empty folder if a folder of the path does not exists
     """
-    if 's3' in folder_path:
+    if 's3://' in folder_path:
         return None
     elif not os.path.exists(folder_path):
         os.makedirs(folder_path)
@@ -111,7 +115,7 @@ def path_join(*args):
     """Joins paths such as folders and file names into a valid path string
     """
     joined = os.path.join(*args)
-    if 's3' in joined:
+    if 's3://' in joined:
         return joined.replace('\\', '/')
     else:
         return joined
@@ -119,7 +123,7 @@ def path_join(*args):
 def read_from_json(path):
     """Reads to a dict a json file if it exists, else will return None
     """
-    if 's3' in path:
+    if 's3:' in path:
         bucket, prefix = _bucket_prefix(path)
         content_object = s3_resource.Object(bucket, prefix)
         try:
@@ -215,26 +219,35 @@ def read_parquets(
         pandas.DataFrame: A pandas dataframe with all the parquet files concatenated
     """
 
+    # If the path is not an s3 path, set the number of threads to 500
     if not any(['s3:' in path, any(['s3:' in _ for _ in path])]):
         threads=500
+
     def _read_parquet_file(file_path, columns=None, retry_number = 0):
         """Read a single parquet file
         """
+        # If the number of retries exceeds the max_retries, log an error and return None
         if retry_number >= max_retries:
             _logs_file_ops.error(f'Failed for {file_path} after {retry_number} retries')
             return None
         try:
+            # Read the parquet file
             parquet_file  = pq.ParquetFile(file_path)
+            # If the number of rows in the file is 0, return None
             if parquet_file.metadata.num_rows == 0:
                 return None
+            # If columns are specified, only read those columns
             if columns is not None:
                 columns = [_ for _ in columns if _ in parquet_file.schema_arrow.names]
                 if len(columns)==0: return None
+            # Return the pandas dataframe
             return (parquet_file.read(columns=columns).to_pandas())
         except Exception as e:
+            # If the exception is an ArrowInvalid, log a warning and return None
             if isinstance(e, ArrowInvalid):
                 _logs_file_ops.warning(f'Skipping invalid parquet file: {file_path}')
                 return None
+            # If the exception is not an ArrowInvalid, log a warning and retry
             _logs_file_ops.warning(f'Retrying: {file_path}: {e}')
             # Full jitter wait
             temp = min(10, 2**retry_number)
@@ -246,30 +259,40 @@ def read_parquets(
     def _read_list_of_parquet_files(file_list: list[str], columns=None):
         """Read a list of parquet files serially using `_read_parquet_file`
         """
+        # Concatenate all the parquet files in the list into a single pandas dataframe
         df = pd.concat([
                 _read_parquet_file(file_path=f, columns=columns)
                 for f in tqdm(file_list, desc = 'Reading...', leave = False)
             ])
+        # If replace_npnan is False, return the dataframe as is
         if replace_npnan is False:
             return df
+        # If replace_npnan is True, replace all np.nan values with the specified value and return the dataframe
         else:
             _logs_file_ops.warning(f'[!] Replace np.nan with {replace_npnan}')
             return df.replace(np.nan, replace_npnan)
 
+    # If the path is a string, check if it's a parquet file or a folder
     if isinstance(path, str):
         if '.parquet' in path:
+            # If it's a parquet file, read it and return the pandas dataframe
             return pd.read_parquet(path=path, columns=columns)
         else:
+            # If it's a folder, get a list of all the parquet files in the folder
             list_of_files = [_ for _ in listdir(path) if '.parquet' in _]
+    # If the path is a list, use the list as the list of parquet files
     elif isinstance(path, list):
         list_of_files = path
 
+    # If there are no parquet files in the list, log a warning and return None
     if len(list_of_files) == 0:
         _logs_file_ops.warning(f'No .parquet files found in {path}')
         return None
 
+    # If threads is specified, distribute the list of parquet files into blocks and read them in parallel
     if threads:
         file_list_of_lists = list_distribute_into_blocks(list_of_files, threads)
+    # If threads is not specified, read the list of parquet files serially
     else:
         file_list_of_lists = [[_] for _ in list_of_files]
     with ThreadPoolExecutor(max_workers=threads) as executor:
@@ -279,4 +302,5 @@ def read_parquets(
         ]
         sublists = [future.result() for future in futures]
 
+    # Concatenate all the sublists into a single pandas dataframe and return it
     return pd.concat(sublists)
--- utils/googlesheets.py
+++ utils/googlesheets.py
@@ -7,45 +7,56 @@ try:
 except:
     pass
 
-
 def gs_read(spreadsheet_id, worksheet_name=None, range_name=None, pandas_df = True):
+    # Authenticate with Google Sheets API
     gc = gspread.oauth(
         credentials_filename='_tokens/google_sheets_api.json',
         authorized_user_filename='_tokens/gs_token.json'
     )
+    # Open the specified spreadsheet
     sh = gc.open_by_key(spreadsheet_id)
+    # Get a list of worksheet names in the spreadsheet
     list_of_sheets = [_.title for _ in sh.worksheets()]
+    # If the specified worksheet exists, open it. Otherwise, use the first sheet in the spreadsheet and log a warning message.
     if worksheet_name is not None and worksheet_name in list_of_sheets:
         ws = sh.worksheet(worksheet_name)
     else:
         ws = sh.sheet1
         _logs_gsheets.warning(f'{worksheet_name} was not found. Using {ws.title} instead')
+    # Get all the data from the worksheet
     data = ws.get_all_records()
+    # If pandas_df is True, return the data as a pandas DataFrame. Otherwise, return the data as a list of dictionaries.
     if pandas_df:
         return pd.DataFrame(data)
     else:
         return data
 
 def gs_write(spreadsheet_id: str, worksheet_name: str, df: pd.DataFrame, append = False):
-    """Writes a pandas.DataFrame to the specified spreadsheet_id/worksheet_name. If `append` is set to `True`, the input dataframe is reshaped to match the columns of the sheet's data. NULL values are replaces with empty strings
+    """
+    Writes a pandas.DataFrame to the specified Google Sheet.
 
     Args:
         spreadsheet_id (str): Spreadsheet ID (can be derived from the URL)
-        worksheet_name (str): name of the worksheet in which to write. If a worksheet with the specified name does not exists, the function will create one with the specified name
-        df (pd.DataFrame): input dataframe
+        worksheet_name (str): Name of the worksheet in which to write. If a worksheet with the specified name does not exist, the function will create one with the specified name.
+        df (pd.DataFrame): Input dataframe
         append (bool, optional): Set to True to append data to existing data after reshape. Set to false to overwrite **all** data in the sheet. Defaults to False.
     """
+    # Authenticate with Google Sheets API
     gc = gspread.oauth(
         credentials_filename='_tokens/google_sheets_api.json',
         authorized_user_filename='_tokens/gs_token.json'
     )
+
+    # Open the specified spreadsheet
     sh = gc.open_by_key(spreadsheet_id)
+    # Get a list of worksheet names in the spreadsheet
     list_of_sheets = [_.title for _ in sh.worksheets()]
+    # If the specified worksheet exists, open it. Otherwise, create a new worksheet with the specified name.
     if worksheet_name in list_of_sheets:
         ws = sh.worksheet(worksheet_name)
     else:
         ws = sh.add_worksheet(title=worksheet_name, rows=len(df), cols=len(df.columns))
-
+    # If append is True, reshape the input dataframe to match the columns of the sheet's data, replace NULL values with empty strings, and append the data to the sheet.
     if append:
         existing_columns = ws.get('1:1')
         existing_columns = pd.DataFrame(existing_columns, columns=existing_columns[0])
@@ -53,6 +64,7 @@ def gs_write(spreadsheet_id: str, worksheet_name: str, df: pd.DataFrame, append 
         df.fillna('', inplace=True)
         towrite = df.values.tolist()
         ws.append_rows(towrite)
+    # If append is False, replace NULL values with empty strings, insert the column names as the first row of the data, clear the sheet, and write the data to the sheet.
     else:
         df.fillna('', inplace=True)
         towrite = df.values.tolist()
